46011cda8717adc035029332ffa14e0d6c5db139
parshvadave <parshva.dave@ucalgary.ca>
2026-02-15 16:22:28 -0700

    finished question 2

--- assign2q1.asm
+++ assign2q1.asm
@@ -1,6 +1,6 @@
 .data
-inputNumber: .string "%ld"
-outputSum: .string "%ld\n"
+input_number: .string "%ld"
+output_sum: .string "%ld\n"
 
 .bss
 .balign 8
@@ -15,19 +15,16 @@ number: .skip 8
 
 
 
-main: stp x29, x30, [sp, #-16]!
+main: 
+stp x29, x30, [sp, #-16]!
 mov x29, sp
 
 
-
-
-ldr x0, =inputNumber
+ldr x0, =input_number
 ldr x1, =number
 bl scanf
 ldr x14, =number
 ldr x19, [x14]
-
-
 
 
 mov x20, #0
@@ -36,7 +33,7 @@ mov x25, #0
 
 
 
-loop:
+sum_loop:
 add x20, x20, #1
 add x21, x20, #1
 add x22, x20, #2
@@ -46,16 +43,14 @@ mul x24, x23, x20
 add x25, x25, x24
 
 cmp x20, x19
-b.lt loop
+b.lt sum_loop
 
 
 
 
-ldr x0, =outputSum
+ldr x0, =output_sum
 mov x1, x25
 bl printf
-
-
 
 
 ldp x29, x30, [sp], #16
--- assign2q2.asm
+++ assign2q2.asm
@@ -1,37 +1,56 @@
 .data
-input: .string "%ld"
-output: .string "%ld\n"
+input_number: .string "%ld"
+output_sum: .string "%ld\n"
 
 .bss
 .balign 8
-n: .skip 8
-
+number: .skip 8
 
 .text
 .extern scanf
 .extern printf
 .global main
 
-main: stp x29, x30, [sp, #-16]!
+
+
+
+
+main: 
+stp x29, x30, [sp, #-16]!
 mov x29, sp
 
-ldr x0, =input
-ldr x1, =n
-bl scanf
 
-ldr x14, =n
+ldr x0, =input_number
+ldr x1, =number
+bl scanf
+ldr x14, =number
 ldr x19, [x14]
 
 
-mul x20, x19, x19
-mov x21, #2
-mov x22, #1
-madd x23, x21, x19, x22
-mul x24, x23, x20
+mov x20, #0
+mov x26, #0
 
 
-ldr x0, =output
-mov x1, x24
+
+
+mov x22, #2
+mov x23, #1
+sum_loop:
+add x20, x20, #1
+mul x21, x20, x20
+madd x24, x20, x22, x23
+mul x25, x24, x21
+
+add x26, x26, x25
+
+cmp x20, x19
+b.lt sum_loop
+
+
+
+
+ldr x0, =output_sum
+mov x1, x26
 bl printf
 
 
