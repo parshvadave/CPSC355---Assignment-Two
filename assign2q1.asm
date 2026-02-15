7a8e83d88b7e08ecfd58cc25eadb243337a7f2e8
parshvadave <parshva.dave@ucalgary.ca>
2026-02-15 15:47:40 -0700

    finished q1

--- assign2q1.asm
+++ assign2q1.asm
@@ -1,36 +1,61 @@
 .data
-input: .string "%ld"
-output: .string "%ld\n"
+inputNumber: .string "%ld"
+outputSum: .string "%ld\n"
 
 .bss
 .balign 8
-n: .skip 8
-
+number: .skip 8
 
 .text
 .extern scanf
 .extern printf
 .global main
 
+
+
+
+
 main: stp x29, x30, [sp, #-16]!
 mov x29, sp
 
-ldr x0, =input
-ldr x1, =n
+
+
+
+ldr x0, =inputNumber
+ldr x1, =number
 bl scanf
+ldr x14, =number
+ldr x19, [x14]
 
-ldr x14, =n
-ldr x19, [x14] //--> value for n
 
 
-add x20, x19, #1 // x20 = n + 1
-add x21, x19, #2// x21 = n + 2
-mul x22, x21, x20 // x22 = (n+2) * (n + 1)
-mul x23, x22, x19 // x23 = n * ((n + 2) * (n + 1))
 
-ldr x0, =output
-mov x1, x23
+mov x20, #0
+mov x25, #0
+
+
+
+
+loop:
+add x20, x20, #1
+add x21, x20, #1
+add x22, x20, #2
+mul x23, x22, x21
+mul x24, x23, x20
+
+add x25, x25, x24
+
+cmp x20, x19
+b.lt loop
+
+
+
+
+ldr x0, =outputSum
+mov x1, x25
 bl printf
+
+
 
 
 ldp x29, x30, [sp], #16
